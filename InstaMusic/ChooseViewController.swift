d06429b82c06a0070602df89df96b9d853efc49f
Anirban Kumar <anisingh2000@gmail.com>
2019-11-20 14:16:50 -0500

    App Store Version

--- InstaMusic/ChooseViewController.swift
+++ InstaMusic/ChooseViewController.swift
@@ -13,17 +13,24 @@ class ChooseViewController: UIViewController {
 
     @IBOutlet weak var segmentedControl: UISegmentedControl!
     @IBOutlet weak var simpleContent: SimpleColorView!
-    @IBOutlet weak var continueButton: UIButton!
+    @IBOutlet weak var postButton: UIButton!
     
-    var titleOfSong : String?
-    var imageOfSong : UIImage?
-    var artistOfSong : String?
+    var song : String?
+    var artwork : UIImage?
+    var artist : String?
     
-    let player = MPMusicPlayerController.systemMusicPlayer
+    let music_player = MPMusicPlayerController.systemMusicPlayer
+
+    var exportImage: UIImage?
+    
+    @IBOutlet weak var noSongPlayingLabel: UILabel!
+
+    let messages : [String] = ["Please go to the Music app and begin playing your music.", "If music is playing and image isn't showing up, please add the song to your library, clear the music app and try again."]
 
     override func viewDidLoad() {
-        super.viewDidLoad()
         
+        super.viewDidLoad()
+        /*
         let status = MPMediaLibrary.authorizationStatus()
         switch status {
             case .authorized:
@@ -48,31 +55,34 @@ class ChooseViewController: UIViewController {
         @unknown default:
             break
         }
+ */
+        screenshotPurposes()
     }
     
     func setUp() {
-        player.beginGeneratingPlaybackNotifications()
+        music_player.beginGeneratingPlaybackNotifications()
         NotificationCenter.default.addObserver(self,selector: #selector(songChanged),name: NSNotification.Name.MPMusicPlayerControllerNowPlayingItemDidChange,object: nil)
     }
-    
+    func screenshotPurposes() {
+        song = "SICKO MODE"
+        artist = "Travis Scott"
+        artwork = UIImage(named: "travis")
+        instantiate(value: true)
+    }
     func getSong() {
-        //print(player.nowPlayingItem?.title)
-        //titleOfSong = trimSong(song: (player.nowPlayingItem?.title)!)
-        titleOfSong = player.nowPlayingItem?.title
-        artistOfSong = player.nowPlayingItem?.artist
-        imageOfSong = player.nowPlayingItem?.artwork?.image(at: simpleContent.songImage.bounds.size)
+        song = music_player.nowPlayingItem?.title
+        artist = music_player.nowPlayingItem?.artist
+        artwork = music_player.nowPlayingItem?.artwork?.image(at: simpleContent.songImage.bounds.size)
         
-        if titleOfSong == nil {
-            simpleContent.isHidden = true
-            segmentedControl.isEnabled = false
-            continueButton.isHidden = true
+        if song == nil {
+            hideAndDisable()
+        } else if artwork == nil {
+            hideAndDisableWithTips()
         } else {
-            titleOfSong = trimSong(song: (player.nowPlayingItem?.title)!)
-            segmentedControl.isEnabled = true
-            continueButton.isHidden = false
+            song = trimSong(songTitle: song!)
+            unhideAndEnable()
             instantiateSimple()
         }
-        
     }
     @objc func songChanged(_ notification: Notification){
         getSong()
@@ -82,7 +92,6 @@ class ChooseViewController: UIViewController {
             instantiateArtworkBackground()
         }
     }
-    
     
     @IBAction func backgroundStyleChanged(_ sender: Any) {
         switch segmentedControl.selectedSegmentIndex {
@@ -94,52 +103,44 @@ class ChooseViewController: UIViewController {
             break
         }
     }
-    @IBAction func nextTapped(_ sender: Any) {
-        let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
-        let newViewController = storyBoard.instantiateViewController(withIdentifier: "saveVC") as! SaveViewController
-    
-        self.present(newViewController, animated: true, completion: nil)
-    }
-    
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?)
-    {
-        let colors = imageOfSong?.getColors()
-
-        if segue.destination is SaveViewController
-        {
-            
-            let renderer = UIGraphicsImageRenderer(size: simpleContent.bounds.size)
-            let export = renderer.image { ctx in
-                simpleContent.drawHierarchy(in: simpleContent.bounds, afterScreenUpdates: true)
-            }
-            let vc = segue.destination as? SaveViewController
-            vc?.imageExport = export
-            vc?.background = colors?.background
-            vc?.textColor = colors?.detail
+    @IBAction func instagramTapped(_ sender: Any) {
+        let renderer = UIGraphicsImageRenderer(size: simpleContent.bounds.size)
+        exportImage = renderer.image { ctx in
+            simpleContent.drawHierarchy(in: simpleContent.bounds, afterScreenUpdates: true)
         }
+        shareToInstagramStories()
     }
-    
+    @IBAction func saveTapped(_ sender: Any) {
+        //let imageData = UIImage.pngData(imageExport!)
+        UIImageWriteToSavedPhotosAlbum(exportImage!, nil, nil, nil)
+        let alert = UIAlertController(title: "Saved", message: "Image saved to camera roll", preferredStyle: .alert)
+        let ok = UIAlertAction(title: "Ok", style: .default, handler: nil)
+        alert.addAction(ok)
+        self.present(alert, animated: true, completion: nil)
+    }
     func instantiateSimple() {
-        instantiate()
-        let colors = imageOfSong?.getColors()
-        
-        simpleContent.backgroundImage.isHidden = true
-        simpleContent.backgroundColor = colors?.background
+        instantiate(value: true)
     }
     func instantiateArtworkBackground() {
-        instantiate()
-
-        simpleContent.backgroundImage.isHidden = false
-        simpleContent.backgroundImage.image = imageOfSong!
-        simpleContent.backgroundImage.blurView.setup(style: UIBlurEffect.Style.light, alpha: 0.99).enable()
-        
-        simpleContent.backgroundImage.contentMode = .scaleAspectFill
+        instantiate(value: false)
     }
     
-    func instantiate() {
+    func instantiate(value: Bool) {
+        if value == true {
+            let colors = artwork?.getColors()
+            
+            simpleContent.backgroundImage.isHidden = true
+            simpleContent.backgroundColor = colors?.background
+        } else {
+            simpleContent.backgroundImage.isHidden = false
+            simpleContent.backgroundImage.image = artwork!
+            simpleContent.backgroundImage.blurView.setup(style: UIBlurEffect.Style.light, alpha: 0.99).enable()
+            
+            simpleContent.backgroundImage.contentMode = .scaleAspectFill
+        }
         simpleContent.isHidden = false
 
-        let colors = imageOfSong?.getColors()
+        let colors = artwork?.getColors()
         
         simpleContent.layer.shadowPath = UIBezierPath(roundedRect: simpleContent.layer.bounds, cornerRadius: 0).cgPath
         simpleContent.layer.shadowColor = colors?.primary.cgColor
@@ -148,9 +149,11 @@ class ChooseViewController: UIViewController {
         simpleContent.layer.shadowRadius = 15
         simpleContent.layer.masksToBounds = false
         
-        simpleContent.artistLabel.text = "by " + artistOfSong!
-        simpleContent.songTitle.text = titleOfSong!
-        simpleContent.songImage.image = imageOfSong!
+        simpleContent.artistLabel.text = "by " + artist!
+        simpleContent.songTitle.text = song!
+        
+        //simpleContent.songImage.image = music_player.nowPlayingItem?.artwork?.image(at: simpleContent.songImage.bounds.size)
+        simpleContent.songImage.image = artwork
         simpleContent.songImage.layer.shadowPath = UIBezierPath(roundedRect: simpleContent.songImage.bounds, cornerRadius: 0).cgPath
         simpleContent.songImage.layer.shadowColor = colors?.detail.cgColor
         simpleContent.songImage.layer.shadowOpacity = 0.5
@@ -161,21 +164,46 @@ class ChooseViewController: UIViewController {
         simpleContent.artistLabel.textColor = colors?.secondary
         simpleContent.appleMusic.textColor = colors?.detail
         
-        continueButton.backgroundColor = colors?.primary
+        postButton.backgroundColor = colors?.primary
     }
-    
-    func trimSong(song: String) -> String {
-        if let index = song.index(of: "(feat.") {
-            let substring = song[..<index]   // ab
+    func hideAndDisable() {
+        simpleContent.isHidden = true
+        segmentedControl.isEnabled = false
+        postButton.isHidden = true
+        noSongPlayingLabel.text = messages[0]
+    }
+    func hideAndDisableWithTips() {
+        simpleContent.isHidden = true
+        segmentedControl.isEnabled = false
+        postButton.isHidden = true
+        noSongPlayingLabel.text = messages[1]
+    }
+    func unhideAndEnable() {
+        simpleContent.isHidden = false
+        segmentedControl.isEnabled = true
+        postButton.isHidden = false
+    }
+    func trimSong(songTitle: String) -> String {
+        if let index = songTitle.index(of: "(feat.") {
+            let substring = songTitle[..<index]   // ab
             let string = String(substring)
             return string
-        } else if let index = song.index(of: "(with") {
-            let substring = song[..<index]   // ab
+        } else if let index = songTitle.index(of: "(with") {
+            let substring = songTitle[..<index]   // ab
             let string = String(substring)
             return string
         }
-        return song
-        
+        return songTitle
+    }
+    private func shareToInstagramStories() {
+        guard let imagePNGData = exportImage!.pngData() else { return }
+        guard let instagramStoryUrl = URL(string: "instagram-stories://share") else { return }
+        guard UIApplication.shared.canOpenURL(instagramStoryUrl) else { return }
+
+        let itemsToShare: [[String: Any]] = [["com.instagram.sharedSticker.backgroundImage": imagePNGData]]
+        let pasteboardOptions: [UIPasteboard.OptionsKey: Any] = [.expirationDate: Date().addingTimeInterval(60 * 5)]
+        UIPasteboard.general.setItems(itemsToShare, options: pasteboardOptions)
+        UIApplication.shared.open(instagramStoryUrl, options: [:], completionHandler: nil)
     }
 }
 extension StringProtocol { // for Swift 4.x syntax you will needed also to constrain the collection Index to String Index - `extension StringProtocol where Index == String.Index`
@@ -209,9 +237,6 @@ extension StringProtocol { // for Swift 4.x syntax you will needed also to const
     }
 }
 extension UIView {
-
-    // Using a function since `var image` might conflict with an existing variable
-    // (like on `UIImageView`)
     func asImage() -> UIImage {
         if #available(iOS 10.0, *) {
             let renderer = UIGraphicsImageRenderer(bounds: bounds)
